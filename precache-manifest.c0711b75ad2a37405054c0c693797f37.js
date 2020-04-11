4ede63ca9327f056ccb729d135854c605926d822
N <Madhuri.N@finastra.com>
2020-04-11 11:56:25 +0530

    Updates

---
+++ precache-manifest.c0711b75ad2a37405054c0c693797f37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bea9cc0f1fc16920bb5771808a279494",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2e1c095a5499aaa3704e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "58c9844f18350f25cac1",
+    "url": "/static/js/2.e6fe620a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e6fe620a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e1c095a5499aaa3704e",
+    "url": "/static/js/main.139905c1.chunk.js"
+  },
+  {
+    "revision": "3de6589858c441ab6e9c",
+    "url": "/static/js/runtime-main.edf3ba06.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
